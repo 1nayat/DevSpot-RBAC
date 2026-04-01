ed753a4d282f36dace6ad51aa1319ff11c968da6
Shahfarhan26 <2468shahfarhan@gmail.com>
2026-04-01 16:46:08 +0530

    migration added

--- Migrations/20260401101526_mig1.Designer.cs
+++ Migrations/20260401101526_mig1.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevSpot.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250129173605_mig2")]
-    partial class mig2
+    [Migration("20260401101526_mig1")]
+    partial class mig1
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
